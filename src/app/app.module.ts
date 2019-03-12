174c8f1b3891d66553f14c73a0ec199745a880f4
nmrahul28 <manikantarahul.n@gmail.com>
2019-03-12 20:22:54 +0530

    files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forms/forms.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+  public user_data: FormGroup;
+  public address: FormGroup;
+
+  constructor() { }
+  ngOnInit() {
+    this.user_data =new FormGroup({
+      employee_name: new FormControl('',Validators.compose([Validators.required, Validators.minLength(5)])),
+      email: new FormControl('',Validators.compose([Validators.required, Validators.email])),
+      phone: new FormControl('',Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])),
+      employee_id: new FormControl('',Validators.compose([Validators.required, Validators.maxLength(4)])),
+      address:new FormGroup({
+        street_name:new FormControl('', Validators.required),
+        city_name:new FormControl('', Validators.required),
+        state_name:new FormControl('',Validators.required),
+        zip:new FormControl('',Validators.compose([Validators.required, Validators.maxLength(6)]))
+      })
+      });
+
+  }
+
+  onSubmit() {
+    console.log('Valid', this.user_data.valid);
+    console.log('Name', this.user_data.value.employee_name);
+    console.log('Email', this.user_data.value.email);
+    console.log('Message', this.user_data.value.phone);
+    console.log(this.user_data.value);
+  }
+
+
+}
