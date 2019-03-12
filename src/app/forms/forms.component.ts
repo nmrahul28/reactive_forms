f64a238a8e9eae828ac716950a91f3f7f1d9499f
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-03-12 23:26:08 +0530

    Update forms.component.ts

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,58 +1,39 @@
-<form [formGroup]="user_data" (ngSubmit)="onSubmit()">
-  <h1>Enter Employee Details</h1>
-  <div class="div_class">
-    <label>Employee Name:</label><br>
-    <input type="text" formControlName="employee_name" placeholder="Name">
-    <div *ngIf="(user_data.controls.employee_name.dirty || user_data.controls.employee_name.touched) && user_data.controls.employee_name.invalid">
-  <div style="color:red" *ngIf="user_data.controls.employee_name.errors.required">
-    Name is required.
-  </div>
-  <div style="color:red" *ngIf="user_data.controls.employee_name.errors.minlength">
-    Name must be at least 5 characters long.
-  </div>
-</div>
-    <br>
-    <label>Email:</label><br>
-    <input type="text" formControlName="email" placeholder="email">
-    <div *ngIf="(user_data.controls.email.dirty || user_data.controls.email.touched) && user_data.controls.email.invalid">
-    <div style="color:red" *ngIf="user_data.controls.email.errors.required">
-        Email must be required.
-    </div>
-    <div style="color:red" *ngIf="(user_data.controls.email.errors.email)">
-    Enter a valid email address.
-    </div>
-  </div>
-    <br>
-    <label>Contact Number:</label><br>
-    <input type="text" formControlName="phone" placeholder="Mobile number">
-    <div *ngIf="(user_data.controls.phone.dirty || user_data.controls.phone.touched) && user_data.controls.phone.invalid">
-    <div style="color: red" *ngIf="user_data.controls.phone.errors.required">
-        Mobile Number is required.
-    </div>
-    <div style="color: red"*ngIf="user_data.controls.phone.errors.minlength ||user_data.controls.phone.errors.maxlength">
-        Length should be of 10 characters.
-      </div>
-  </div>
-    <br>
-    <label>ID:</label><br>
-    <input type="text" formControlName="employee_id" placeholder="id">
-    <div *ngIf="(user_data.controls.employee_id.dirty || user_data.controls.employee_id.touched) && user_data.controls.employee_id.invalid">
-        <div style="color: red" *ngIf="user_data.controls.employee_id.errors.required">
-            Mobile Number is required.
-        </div>
-        <div style="color: red"*ngIf="user_data.controls.employee_id.errors.minlength || user_data.controls.employee_id.errors.maxlength">
-            Length should be of 4 characters.
-          </div>
-      </div>
-    <br>
-    <div formGroupName="address">
-      <label>Address:</label><br>
-      <input type="text" formControlName="street_name" placeholder="street"><br><br>
-      <input type="text" formControlName="city_name" placeholder="city"><br><br>
-      <input type="text" formControlName="state_name" placeholder="state"><br><br>
-      <input type="text" formControlName="zip" placeholder="zip">
-      <br><br>
-    </div>
-    <button type="submit" [disabled]="!user_data.valid">Submit</button>
-  </div>
-</form>
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+  public user_data: FormGroup;
+  public address: FormGroup;
+
+  constructor() { }
+  ngOnInit() {
+    this.user_data =new FormGroup({
+      employee_name: new FormControl('',Validators.compose([Validators.required, Validators.minLength(5)])),
+      email: new FormControl('',Validators.compose([Validators.required, Validators.email])),
+      phone: new FormControl('',Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('/^[6-9]\d{9}$/')])),
+      employee_id: new FormControl('',Validators.compose([Validators.required, Validators.maxLength(4), Validators.minLength(4)])),
+      address:new FormGroup({
+        street_name:new FormControl('', Validators.required),
+        city_name:new FormControl('', Validators.required),
+        state_name:new FormControl('',Validators.required),
+        zip:new FormControl('',Validators.compose([Validators.required, Validators.maxLength(6), Validators.minLength(6)]))
+      })
+      });
+
+  }
+
+  onSubmit() {
+    console.log('Valid', this.user_data.valid);
+    console.log('Name', this.user_data.value.employee_name);
+    console.log('Email', this.user_data.value.email);
+    console.log('Message', this.user_data.value.phone);
+    console.log(this.user_data.value);
+  }
+
+
+}
