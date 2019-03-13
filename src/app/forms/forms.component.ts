fd1d6d01b7d3b7c8c303da97f37eb5b186926a07
Manikanta Rahul <manikantarahul.n@gmail.com>
2019-03-13 15:07:16 +0530

    Update forms.component.ts

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -15,24 +15,27 @@ export class FormsComponent implements OnInit {
     this.user_data =new FormGroup({
       employee_name: new FormControl('',Validators.compose([Validators.required, Validators.minLength(5)])),
       email: new FormControl('',Validators.compose([Validators.required, Validators.email])),
-      phone: new FormControl('',Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('/^[6-9]\d{9}$/')])),
+      phone: new FormControl('',Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('[0-9]+')])),
       employee_id: new FormControl('',Validators.compose([Validators.required, Validators.maxLength(4), Validators.minLength(4)])),
       address:new FormGroup({
         street_name:new FormControl('', Validators.required),
         city_name:new FormControl('', Validators.required),
         state_name:new FormControl('',Validators.required),
-        zip:new FormControl('',Validators.compose([Validators.required, Validators.maxLength(6), Validators.minLength(6)]))
+        zip:new FormControl('',Validators.compose([Validators.required, Validators.maxLength(6), Validators.minLength(6),Validators.pattern('[0-9]+')]))
       })
       });
 
   }
+  get control(){
+    return this.user_data.controls;
+  }
 
   onSubmit() {
-    console.log('Valid', this.user_data.valid);
-    console.log('Name', this.user_data.value.employee_name);
-    console.log('Email', this.user_data.value.email);
-    console.log('Message', this.user_data.value.phone);
-    console.log(this.user_data.value);
+    // console.log('Valid', this.user_data.valid);
+    // console.log('Name', this.user_data.value.employee_name);
+    // console.log('Email', this.user_data.value.email);
+    // console.log('Message', this.user_data.value.phone);
+    alert(JSON.stringify(this.user_data.value));
   }
 
 
